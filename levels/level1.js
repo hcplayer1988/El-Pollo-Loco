40732b616b3773ec19777bb0c7a4ce0881f0d1b7
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-07-23 16:01:21 +0200

    add small chicken

--- levels/level1.js
+++ levels/level1.js
@@ -3,7 +3,13 @@ const level1 = new Level(
     new Chicken(),
     new Chicken(),
     new Chicken(),
+    new Chicken(),
+    new Chicken(),
     new Endboss(),
+    new SmallChicken(),
+    new SmallChicken(),
+    new SmallChicken(),
+    new SmallChicken(),
     ],
     [
     new Cloud(),
--- models/smallChicken.class.js
+++ models/smallChicken.class.js
@@ -1,21 +1,20 @@
-class Chicken extends MovableObject {
-    y = 380;
-    width = 50;
-    height = 50;
+class SmallChicken extends MovableObject {
+    y = 395;
+    width = 30;
+    height = 30;
     
 
     images_walking = [
-            'assets/img/ingame_imgs/3.enemies.chicken/chicken_normal/1_walk/1_w.png',
-            'assets/img/ingame_imgs/3.enemies.chicken/chicken_normal/1_walk/2_w.png',
-            'assets/img/ingame_imgs/3.enemies.chicken/chicken_normal/1_walk/3_w.png',
-
+            'assets/img/ingame_imgs/3.enemies.chicken/chicken_small/1_walk/1_w.png',
+            'assets/img/ingame_imgs/3.enemies.chicken/chicken_small/1_walk/2_w.png',
+            'assets/img/ingame_imgs/3.enemies.chicken/chicken_small/1_walk/3_w.png',
         ];
         
 
     constructor() {
         super().loadImage('assets/img/ingame_imgs/3.enemies.chicken/chicken_normal/1_walk/1_w.png')
         this.loadImages(this.images_walking);
-        this.x = 950 + Math.random() * 1500; // sorgt dafür das die Hühnchen immer an einer zufälligen stellle auf der xachse zwischen 200 und 700 pixeln erscheinen
+        this.x = 650 + Math.random() * 300; // sorgt dafür das die Hühnchen immer an einer zufälligen stellle auf der xachse zwischen 200 und 700 pixeln erscheinen
         this.speed = 0.03 + Math.random() * 0.20; // sorgt dafür das sich die geschwindigkeit jedes Hühnchens zufällig zwischen 0.03 und 0.28 bewegt
         this.animate();
     }
