9dfbd65f3d37381d69e6e3d6ac72f6915fe96f4d
Falko <162439151+hcplayer1988@users.noreply.github.com>
2025-08-05 08:37:04 +0200

    refactoring character code

--- models/Character.claas.js
+++ models/Character.claas.js
@@ -44,7 +44,19 @@ class Character extends MovableObject {
         'assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-7.png',
         'assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-8.png',
         'assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-9.png',
-        'assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-10.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-10.png'
+    ];
+    images_idle_long = [
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-11.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-12.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-13.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-14.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-15.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-16.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-17.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-18.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-19.png',
+        'assets/img/ingame_imgs/2.character.pepe/1.idle/long_idle/I-20.png'
     ];
 
     walking_sound = new Audio('audio/fast_walk_sand.mp3');
@@ -61,52 +73,114 @@ class Character extends MovableObject {
     };
 
     constructor() {
-        super().loadImage('assets/img/ingame_imgs/2.character.pepe/2.walk/W-21.png')
+        super().loadImage('assets/img/ingame_imgs/2.character.pepe/1.idle/idle/I-1.png')
         this.loadImages(this.images_walking);
         this.loadImages(this.images_jumping);
         this.loadImages(this.images_dead);
         this.loadImages(this.images_hurt);
+        this.loadImages(this.images_idle);
+        this.loadImages(this.images_idle_long);
         this.applyGravity();
         this.animate();
+        this.lastActionTime = Date.now();
+        this.idleLongPlayed = false;
+        this.idleLongStartTime = null;
     }
 
     animate() {
-        setInterval( () => {
-            this.lastBottomBeforeHit = this.y + this.height;
-            this.walking_sound.pause();
-            // walking right
-            if (this.world.keyboard.d_right && this.x < this.world.level.level_end_x) {
-                this.moveRight();
-                this.otherDirection = false;
-                this.walking_sound.play();
-            }
-            // walking left
-            if (this.world.keyboard.a_left && this.x > -600) {
-                this.moveLeft();
-                this.otherDirection = true;
-                this.walking_sound.play();
-            }
-            
-            // jumping
-            if(this.world.keyboard.w_jump && !this.isAboveGround()) {
-                this.jump();
-            }
-            this.world.camera_x = -this.x + 100;
+        setInterval(() => {
+            this.handleMovement();
+            this.updateCamera();
+            this.resetIdleTimerIfActive();
         }, 1000 / 60);
 
         setInterval(() => {
-            if (this.hurtAnimationTriggered) {
-                this.playAnimation(this.images_hurt);
-            } else if (this.isDead()) {
-                this.playAnimation(this.images_dead);
-            } else if (this.isAboveGround()) {
-                this.playAnimation(this.images_jumping);
-            } else {
-                if (this.world.keyboard.d_right || this.world.keyboard.a_left) {
-                    this.playAnimation(this.images_walking);
-                }
-            }   
+            this.handleHurtOrDeathAnimation() || this.handleMovementAnimation();
         }, 1000 / 25);
+
+        setInterval(() => {
+            this.handleIdleAnimation();
+        }, 1000 / 10);
+}
+
+
+    handleMovement() {
+    this.lastBottomBeforeHit = this.y + this.height;
+    this.walking_sound.pause();
+    this.actionOccurred = false;
+
+    if (this.world.keyboard.d_right && this.x < this.world.level.level_end_x) {
+        this.moveRight();
+        this.otherDirection = false;
+        this.walking_sound.play();
+        this.actionOccurred = true;
+    }
+
+    if (this.world.keyboard.a_left && this.x > -600) {
+        this.moveLeft();
+        this.otherDirection = true;
+        this.walking_sound.play();
+        this.actionOccurred = true;
+    }
+
+    if (this.world.keyboard.w_jump && !this.isAboveGround()) {
+        this.jump();
+        this.actionOccurred = true;
+        }
+    }
+
+    updateCamera() {
+        this.world.camera_x = -this.x + 100;
+    }
+
+    resetIdleTimerIfActive() {
+        if (this.actionOccurred || this.hurtAnimationTriggered || this.isDead()) {
+            this.lastActionTime = Date.now();
+            this.idleLongPlayed = false;
+            this.idleLongStartTime = null;
+        }
+    }
+
+    handleAnimation() {
+        this.handleHurtOrDeathAnimation() ||
+        this.handleMovementAnimation() ||
+        this.handleIdleAnimation();
+    }
+
+    handleHurtOrDeathAnimation() {
+        if (this.hurtAnimationTriggered) {
+            this.playAnimation(this.images_hurt);
+            return true;
+        }
+        if (this.isDead()) {
+            this.playAnimation(this.images_dead);
+            return true;
+        }
+        return false;
+    }
+
+    handleMovementAnimation() {
+        if (this.isAboveGround()) {
+            this.playAnimation(this.images_jumping);
+            return true;
+        }
+        if (this.world.keyboard.d_right || this.world.keyboard.a_left) {
+            this.playAnimation(this.images_walking);
+            return true;
+        }
+        return false;
+    }
+
+    handleIdleAnimation() {
+        let now = Date.now();
+        let inactiveTime = now - this.lastActionTime;
+
+        if (inactiveTime > 5000 || this.idleLongPlayed) {
+            this.playAnimation(this.images_idle_long);
+            this.idleLongPlayed = true;
+        } else {
+            this.playAnimation(this.images_idle);
+        }
     }
 
 }
